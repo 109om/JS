c5c409b67cfdf5c37bb2c96cb9737a3a536d3138
_omkar.x <139672286+109om@users.noreply.github.com>
2026-07-21 07:00:47 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";//treat all JS code as newer version
+//alert("3 + 3") we are using nodejs, not broswer
+
+console.log(3 +
+     3)// code rellability must be high
+
+console.log("Janhavi")
+
+let name = "Janhavi"
+let age = 22
+let isLogin = false
+
+// number => 2 tp power 53
+//bigint
+// string
+// boolean => true/false
+// null
+//undefined
+//symbol use for unique
+
+// object
+
+console.log(typeof "omkar");
+
+console.log(typeof null);//object
+
+console.log(typeof undefined)// undefined
